bc7c974e170893c36d0cb74c59eacffb3429ad9d
batir <batir.vik@gmail.com>
2024-09-08 12:31:23 +0200

    fix bug [page.html copy-btn don't copies content]

--- app/static/js/page.js
+++ app/static/js/page.js
@@ -2,6 +2,6 @@ const copyContentBtn = document.getElementById("copy-content-btn");
 const pageContentDiv = document.getElementById("page-content");
 
 copyContentBtn.onclick = async () => {
-  await navigator.clipboard.writeText(pageContentDiv.innerText);
+  await navigator.clipboard.writeText(pageContentDiv.value);
   alert("Content copied to clipboard");
 };
